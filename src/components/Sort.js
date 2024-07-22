d4c21d699e93c1b968a7ec8b0d931054818cc6aa
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-22 14:55:53 +0530

    Update Sort.js

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,9 +1,74 @@
 import React from 'react'
-
+import styled from 'styled-components';
+import { BsFillGridFill,BsList } from "react-icons/bs";
+import {useFilterContext} from "../context/filter_context";
 const Sort = () => {
+  const {filter_products,grid_view,setGridView,setListView}=useFilterContext();
   return (
-    <div>Sort</div>
+    <Wrapper className='sort-section'>
+      <div className='sorting-list--grid'>
+       <button className={grid_view ? "active sort-btn" :"sort-btn"}
+       onClick={setGridView}>
+        <BsFillGridFill/>
+       </button>
+       <button className={!grid_view ? "active sort-btn" :"sort-btn"}
+       onClick={setListView}>
+        <BsList/>
+       </button>
+      </div>
+      <div className='product-data'>
+       <p>
+        {`${filter_products.length} Product Available`}
+       </p>
+
+      </div>
+      <div className='sort-selection'>
+        dropdown
+
+      </div>
+
+
+    </Wrapper>
   )
-}
+};
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5rem;
+
+  .sorting-list--grid {
+    display: flex;
+    gap: 2rem;
+
+    .sort-btn {
+      padding: 0.8rem 1rem;
+      border: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+    }
+
+    .icon {
+      font-size: 1.6rem;
+    }
+    .active {
+      background-color: ${({ theme }) => theme.colors.black};
+      color: #fff;
+    }
+  }
+
+  .sort-selection .sort-selection--style {
+    padding: 0.5rem;
+    cursor: pointer;
+
+    .sort-select--option {
+      padding: 0.5rem 0;
+      cursor: pointer;
+      height: 2rem;
+      padding: 10px;
+    }
+  }
+`;
 
 export default Sort
