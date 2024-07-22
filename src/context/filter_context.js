703d0ac8c7d4339b59d91e3d63c4f92af98b6c8d
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-22 14:56:14 +0530

    Update filter_context.js

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -21,13 +21,20 @@ export const FilterContextProvider = ({ children }) => {
     return dispatch({ type: "SET_GRID_VIEW" });
   };
 
+   // to set the list view
+   const setListView = () => {
+    return dispatch({ type: "SET_LIST_VIEW" });
+  };
+
+
+
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
     <FilterContext.Provider
-      value={{ ...state, setGridView }}>
+      value={{ ...state, setGridView ,setListView}}>
       {children}
     </FilterContext.Provider>
   );
