12de3ffb347a784f831331797144b437d1feee54
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-23 16:08:41 +0530

    Update filterReducer.js

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -13,11 +13,54 @@ const filterReducer = (state, action) => {
           grid_view: true,
         };
 
+
+
         case "SET_LIST_VIEW":
           return{
             ...state,
             grid_view:false,
           }
+
+        case "GET_SORT_VALUE":
+          // let userSortValue=document.getElementById("sort");
+          // let sort_value=userSortValue.options[userSortValue.selectedIndex].value;
+          // console.log(sort_value)
+          return{
+            ...state,
+            sorting_value:action.payload,
+          }
+        
+          case "SORTING_PRODUCTS":
+            let newSortData;
+           
+      
+            const{filter_products,sorting_value}=state;
+            let tempSortProducts=[...filter_products];
+  
+            const sortingProducts=(a,b)=>{
+              if(sorting_value==="lowest"){
+                return a.price-b.price;
+              }
+              if(sorting_value==="highest"){
+                return b.price-a.price;
+              }
+              if(sorting_value==="a-z"){
+                return a.name.localeCompare(b.name);
+              }
+              if(sorting_value==="z-a")
+              {
+                return b.name.localeCompare(a.name);
+              }
+
+              
+            };
+
+            newSortData=tempSortProducts.sort(sortingProducts);
+
+            return{
+              ...state,
+              filter_products:newSortData,
+            }
   
       default:
         return state;
