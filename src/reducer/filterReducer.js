7fd422a96d734fa85dcfb3d8c2232fee5bc49e88
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-27 16:21:48 +0530

    Update filterReducer.js

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,10 +1,24 @@
 const filterReducer = (state, action) => {
     switch (action.type) {
       case "LOAD_FILTER_PRODUCTS":
+        let priceArr=action.payload.map((curElem)=>curElem.price);
+        console.log(priceArr)
+        // console.log(Math.max.apply(null,priceArr));
+        
+        // let maxprice=priceArr.reduce((initialVal,curVal)=>
+        // Math.max(initialVal,curVal),0);
+
+        // console.log(maxprice);
+
+        let maxprice=Math.max(...priceArr);
+        console.log(maxprice);
+        
+        
         return {
           ...state,
           filter_products: [...action.payload],
           all_products: [...action.payload],
+          filters:{...state.filters,maxprice:maxprice,price:maxprice},
         };
   
       case "SET_GRID_VIEW":
@@ -61,6 +75,78 @@ const filterReducer = (state, action) => {
               ...state,
               filter_products:newSortData,
             }
+
+      case "UPDATE_FILTER_VALUE":
+        const{name,value}=action.payload;
+        return{
+          ...state,
+          filters:{
+            ...state.filters,
+            [name]:value,
+          },
+        }
+
+      case "FILTER_PRODUCTS":
+        let {all_products}=state;
+        let tempFilterProduct=[...all_products];
+        const {text,category,company,color,price}=state.filters;
+        
+        if(text){
+          tempFilterProduct =tempFilterProduct.filter((curElem)=>{
+            return curElem.name.toLowerCase().includes(text);
+          })
+        }
+
+        if(category !== "all"){
+          tempFilterProduct=tempFilterProduct.filter((curElem)=>{
+            return curElem.category === category;
+          })
+        }
+
+        if(company !== "all"){
+          tempFilterProduct=tempFilterProduct.filter((curElem)=>{
+            return curElem.company.toLowerCase() === company.toLowerCase();
+          })
+        }
+
+        if(color !=="all"){
+          tempFilterProduct=tempFilterProduct.filter((curElem)=>{
+           return curElem.colors.includes(color);
+          })
+
+        }
+
+        if(price===0 ){
+          tempFilterProduct=tempFilterProduct.filter(
+            (curElem)=>curElem.price===price)
+        }
+        else
+        {
+          tempFilterProduct=tempFilterProduct.filter(
+            (curElem)=>curElem.price<=price)
+        }
+ 
+
+        return{
+         ...state,
+         filter_products:tempFilterProduct,
+
+        }
+
+  case "CLEAR_FILTERS":
+    return{
+      ...state,
+      filters:{
+        ...state.filter,
+        text:"",
+        category:"all",
+        company:"all",
+        color:"all",
+        maxprice:0,
+        minprice:state.filters.maxprice,
+        price:state.filters.maxprice,
+      }
+    }
   
       default:
         return state;
