06b4e62d9b0b8ca244ed205f71153a236db6132f
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-28 13:09:20 +0530

    Update AddToCart.js

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,8 +5,11 @@ import { FaCheck } from 'react-icons/fa';
 import CartAmountToggle from './CartAmountToggle';
 import { NavLink } from 'react-router-dom';
 import {Button} from '../styles/Button';
+import { useCartContext } from '../context/cart_context';
 
 const AddToCart = ({product}) => {
+    const {addToCart}=useCartContext();
+
     const{id,colors,stock}=product;
     const[color,setColor]=useState(colors[0]);
     const [amount,setAmount]=useState(1);
@@ -45,7 +48,8 @@ const AddToCart = ({product}) => {
         <CartAmountToggle amount={amount}
         setDecrease={setDecrease} setIncrease={setIncrease}/>
 
-        <NavLink to="/cart">
+        <NavLink to="/cart"
+        onClick={()=>addToCart(id,color,amount,product)}>
         <Button className="btn">Add To Cart</Button>
         </NavLink>
 
